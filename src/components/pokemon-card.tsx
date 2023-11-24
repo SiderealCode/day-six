6ef304a422731a17bc1175c786f5b9e0be16f18f
Johann <johann.e.arteaga@gmail.com>
2023-11-24 00:16:54 -0500

    small styling changes

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -6,14 +6,14 @@ interface PokemonCardProps {
 
 export function PokemonCard({ name }: PokemonCardProps) {
   return (
-    <div
-      //href={name}
-      className="group rounded-lg border border-transparent m-3 px-5 py-4 transition-colors dark:border-gray-500 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+    <a
+      href={name}
+      className="py-4 px-5 mx-1 my-1 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
       key={name + "Card"}
     >
       <h2 className={`text-2xl font-semibold`}>
         {name.charAt(0).toUpperCase() + name.slice(1)}
       </h2>
-    </div>
+    </a>
   );
 }
