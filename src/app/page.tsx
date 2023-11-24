d3d89a308af0d6e5ff8150e8ddcce10fe4ad2ac8
Johann <johann.e.arteaga@gmail.com>
2023-11-24 00:05:47 -0500

    added text filter, styling and redid API fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,21 @@
-"use client";
 import { useState, useEffect } from "react";
+import { PokemonGrid } from "@/components/pokemon-grid";
+export default async function Pokedex() {
+  const POKEMON_API = "https://pokeapi.co/api/v2/";
 
-export default function Pokedex() {
-  const [pokemons, setPokemons] = useState([]);
+  // getPokemonList -> Get the first 151 pokemon
+  async function getPokemonList() {
+    const response = await fetch(POKEMON_API + "pokemon?limit=151&offset=0");
+    const data = await response.json();
+    return data.results;
+  }
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon?limit=151"
-      );
-      const data = await response.json();
+  const pokemonList: string[] = await getPokemonList();
 
-      setPokemons(data.results);
-    };
-
-    fetchData();
-  }, []);
-
-  return (
-    <ul>
+  return <PokemonGrid pokemonList={pokemonList} />;
+  /* <ul>
       {pokemons.map(({ name, url }) => (
         <li key={url}>{name}</li>
       ))}
-    </ul>
-  );
+    </ul> */
 }
---
+++ src/components/pokemon-grid.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { useState } from "react";
+import { PokemonCard } from "./pokemon-card";
+
+interface PokemonGridProps {
+  pokemonList: string[];
+}
+
+export function PokemonGrid({ pokemonList }: PokemonGridProps) {
+  const [searchText, setSearchText] = useState("");
+
+  console.log(pokemonList);
+
+  const searchFilter = (pokemonList: string[]) => {
+    return pokemonList.filter((pokemon: any) =>
+      pokemon.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+  };
+
+  const filteredPokemonList = searchFilter(pokemonList);
+
+  return (
+    <>
+      <div>
+        <h3 className="text-2xl py-6 text-center">
+          {" "}
+          Search for your Pokemon!{" "}
+        </h3>
+        <div className="grid w-full max-w-sm items center gap-1.5">
+          <label htmlFor="pokemonName">Pokemon Name</label>
+          <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            type="text"
+            value={searchText}
+            id="pokemonName"
+            placeholder="Charizard, Pikachu, etc."
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+        </div>
+        <h3 className="text-3xl pt-12 pb-6 text-center">Pokemon List</h3>
+      </div>
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
+        {filteredPokemonList.map((pokemon: any) => {
+          return (
+            <PokemonCard name={pokemon.name} key={pokemon.name + "Card"} />
+          );
+        })}
+      </div>
+    </>
+  );
+}
